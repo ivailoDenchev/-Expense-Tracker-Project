9b887bf09c79c547714d09104fa3229e42ac0d3c
Ивайло Денчев <id86254403@edu.mon.bg>
2025-02-22 21:58:15 +0200

    add Model for : Category and Transaction

---
+++ CookingListWebAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CookingListWebAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
