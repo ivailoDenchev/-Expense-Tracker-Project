e27ad928e3365e8ee712a3aaa43cb835497c0f75
Ивайло Денчев <id86254403@edu.mon.bg>
2025-02-23 08:22:01 +0200

    Front-end update

--- CookingListWebAsp/Controllers/TransactionController.cs
+++ CookingListWebAsp/Controllers/TransactionController.cs
@@ -6,38 +6,33 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CookingListWebAsp.Models;
-
 namespace CookingListWebAsp.Controllers
 {
     public class TransactionController : Controller
     {
         private readonly ApplicationDbContext _context;
-
         public TransactionController(ApplicationDbContext context)
         {
             _context = context;
         }
-
         // GET: Transaction
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Transactions.Include(t => t.Category);
             return View(await applicationDbContext.ToListAsync());
         }
-
         // GET: Transaction/Create
-        public IActionResult AddOrEdit()
+        public IActionResult Create()
         {
             ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId");
             return View();
         }
-
         // POST: Transaction/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddOrEdit([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
+        public async Task<IActionResult> Create([Bind("TransactionId,CategoryId,Amount,Note,Date")] Transaction transaction)
         {
             if (ModelState.IsValid)
             {
@@ -48,7 +43,6 @@ namespace CookingListWebAsp.Controllers
             ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "CategoryId", transaction.CategoryId);
             return View(transaction);
         }
-
         // POST: Transaction/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
@@ -59,7 +53,6 @@ namespace CookingListWebAsp.Controllers
             {
                 _context.Transactions.Remove(transaction);
             }
-
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
